be2405d4163417453adffbc742a7022c627f4257
Dyla n ra y <technicalpro@securemedia.ch>
2020-01-23 01:12:31 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,9 @@
 def square_array(array)
-square_array (array) [0, 1, 2, 3]
-count = 0   
-while [0**2, 1**2, 2**2]
+results= []
+counter = 0
+while counter < array.size 
+results<<array[counter]*array
+[counter] 
+counter += 1
+end
 end
